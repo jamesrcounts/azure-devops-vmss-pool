b4e85984d535b6f15c2fac7245f81e81a702a6b3
Jim Counts <jamesrcounts@outlook.com>
2021-03-10 13:23:20 +0000

    Add github service connection

--- infrastructure/azure-devops/build-definition.tf
+++ infrastructure/azure-devops/build-definition.tf
@@ -1,11 +1,16 @@
 resource "azuredevops_build_definition" "build" {
   project_id = azuredevops_project.project.id
   name       = local.project
-  
+
+  ci_trigger {
+    use_yaml = true
+  }
+
   repository {
-    branch_name = "main"
-    repo_id     = "jamesrcounts/azure-devops-vmss-pool"
-    repo_type   = "GitHub"
-    yml_path    = "agents/azure-pipelines.yml"
+    branch_name           = "main"
+    repo_id               = "jamesrcounts/azure-devops-vmss-pool"
+    repo_type             = "GitHub"
+    service_connection_id = azuredevops_serviceendpoint_github.endpoint.id
+    yml_path              = "agents/azure-pipelines.yml"
   }
 }
