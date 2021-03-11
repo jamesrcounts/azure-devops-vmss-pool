0568eb4585ae992ccaaa7c116929b84cdaff09ad
Jim Counts <jamesrcounts@outlook.com>
2021-03-10 20:45:30 -0800

    Add Variable Group (#2)

--- infrastructure/azure-devops/versions.tf
+++ infrastructure/azure-devops/versions.tf
@@ -2,6 +2,10 @@ terraform {
   required_version = ">= 0.14"
 
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.32"
+    }
     azuredevops = {
       source  = "microsoft/azuredevops"
       version = "~> 0.1.0"
@@ -17,6 +21,27 @@ terraform {
 
     workspaces {
       name = "azure-devops-vmss-pool-azdo"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      recover_soft_deleted_key_vaults = true
+      purge_soft_delete_on_destroy    = true
+    }
+
+    template_deployment {
+      delete_nested_items_during_deletion = true
+    }
+
+    virtual_machine {
+      delete_os_disk_on_deletion = true
+    }
+
+    virtual_machine_scale_set {
+      roll_instances_when_required = false
     }
   }
 }
