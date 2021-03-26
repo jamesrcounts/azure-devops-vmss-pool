4ea56feb630aa6f3a97242cee1373735f40f1a09
Jim Counts <jamesrcounts@outlook.com>
2021-03-26 11:33:37 +0000

    Add vmss with custom image

--- agents/infrastructure/keyvault-ops.tf
+++ agents/infrastructure/keyvault-ops.tf
@@ -1,10 +1,12 @@
 locals {
-  ops_secrets = {
-    "agents-subnet-id"            = 1
-    "disk-encryption-set-id"      = 1
-    "log-analytics-workspace-id"  = 1
-    "log-analytics-workspace-key" = 1
-  }
+  ops_secrets =[
+    "agents-subnet-id",            
+    "disk-encryption-set-id",      
+    "log-analytics-workspace-id",  
+    "log-analytics-workspace-key", 
+    "rg-images",
+    "sig-name",
+  ]
 }
 
 data "azurerm_key_vault" "ops" {
@@ -13,7 +15,7 @@ data "azurerm_key_vault" "ops" {
 }
 
 data "azurerm_key_vault_secret" "secrets" {
-  for_each = local.ops_secrets
+  for_each = toset(local.ops_secrets)
 
   name         = each.key
   key_vault_id = data.azurerm_key_vault.ops.id
